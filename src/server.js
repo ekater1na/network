e752d9b0462b1c6d0d824ed91949364226fa9ece
ekater1na <ekaterina_l@tut.by>
2019-11-19 13:25:19 +0300

    build: build basic project structure

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+app.get('/api', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1234');
+  res.json({ ok: true });
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
